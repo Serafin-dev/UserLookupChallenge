2a030615d5baa93716088f25aaa16ba21cb744d7
Serafin-dev <82891246+Serafin-dev@users.noreply.github.com>
2023-05-01 20:02:44 -0300

    Delete nicolas-mateoli-backend directory

--- nicolas-mateoli-backend/src/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const userRoutes = require("./routes/userRoutes");
-module.exports = app;
-
-// use userRoutes for all user-related endpoints
-app.use(userRoutes);
